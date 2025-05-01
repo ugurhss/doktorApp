52f2253b1118b77ce76a75b8faf64a7c9914e24b
ugurhs <ugurcandgn46@icloud.com>
2025-05-01 19:36:03 +0300

    dil

--- .history/resources/views/profile.blade_20250426180803.php
+++ .history/resources/views/profile.blade_20250426180803.php
@@ -21,7 +21,6 @@
 
             <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    h
                     <livewire:profile.delete-user-form />
                 </div>
             </div>
